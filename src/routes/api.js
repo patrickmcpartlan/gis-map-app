ecddd7c72efacd9784a2e8c4fbeebc93084f8e9a
Patrick McPartlan <60454641+patrickmcpartlan@users.noreply.github.com>
2025-05-06 17:01:15 -0500

    correct the api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,13 +1,9 @@
 // src/routes/api.js
 const express = require('express');
 const router = express.Router();
-const featureController = require('../controllers/featureController');
+const featuresController = require('../controllers/featuresController');
 
 // Route to get features from ArcGIS feature service
-// Make sure this is a simple path pattern, not a full URL
-router.get('/features', featureController.getFeatures);
-
-// DO NOT use full URLs in route definitions like this:
-// router.get('https://git.new/pathToRegexpError', ...) - THIS WOULD CAUSE THE ERROR
+router.get('/features', featuresController.getFeatures);
 
 module.exports = router;
